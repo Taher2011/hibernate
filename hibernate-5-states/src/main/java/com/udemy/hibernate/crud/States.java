adf50d9035d92327b08470c38c017b605affad34
Taher2011 <taher.jit@gmail.com>
2021-11-07 15:35:05 +0530

    Hibernate states

---
+++ hibernate-5-states/src/main/java/com/udemy/hibernate/crud/States.java
@@ -0,0 +1,71 @@
+package com.udemy.hibernate.crud;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.udemy.entity.Student;
+import com.udemy.sessionfactory.SessionFactoryUtil;
+
+public class States {
+
+	public static void main(String[] args) {
+
+		// get session factory
+		SessionFactory sessionFactory = SessionFactoryUtil.getSessionFactory();
+
+		// create session
+		Session session = sessionFactory.openSession();
+
+		try {
+			// create a student object
+			Student student1 = new Student("Tushar", "Khanna", "tushar@gmail.com"); // transient state i.e object is
+																					// neither
+																					// associated with session nor
+																					// database.
+
+			// start a transaction
+			Transaction trx = session.beginTransaction();
+
+			// save the student object
+			session.save(student1); // persistent state i.e. object is associated with session and database both.
+
+			student1.setEmail("tushar@yahoo.com"); // persistent state i.e. object is associated with session and
+													// database both.
+
+			// commit transaction
+			trx.commit();
+
+			// close session
+			session.close(); // detach state i.e. once session is closed object is not associated with
+								// session but with database.
+
+			session = sessionFactory.openSession();
+
+			trx = session.beginTransaction();
+
+			Student student2 = session.get(Student.class, 1);
+
+			student2.setEmail("tushar@gmail.com");
+
+			session.update(student2); // detach to persistent state i.e. once session is opened object is associated
+										// with session and database.
+
+			session.delete(student2); // removed state i.e object is associated with session but not with database.
+
+			trx.commit();
+
+			System.out.println();
+
+		} finally {
+			if (session.isOpen()) {
+				// close session
+				session.close(); // detach state i.e. once session is closed object is not associated with
+									// session but with database.
+			}
+			sessionFactory.close();
+		}
+
+	}
+
+}
---
+++ hibernate-5-states/src/main/java/com/udemy/jdbc/DBConnection.java
@@ -0,0 +1,28 @@
+package com.udemy.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/hb_basic?useSSL=false&serverTimezone=UTC";
+		String user = "root";
+		String password = "root";
+
+		try {
+			System.out.println("Connecting to database: " + url);
+
+			Connection connection = DriverManager.getConnection(url, user, password);
+
+			if (connection != null) {
+				System.out.println("Connection successful");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
